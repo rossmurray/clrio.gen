45ff2dc6960e18e56c55ef1452fac0a530ad1e3d
rossmurray <rm@egoorb.com>
2016-10-30 17:47:03 -0700

    change texture gen example

--- src/scene/textureGen.js
+++ src/scene/textureGen.js
@@ -11,13 +11,28 @@ export function draw(screen) {
         max: 1,
         frequency: 1/151
     });
+    const distance = distanceFromCenter(width, height);
     const imageData = noiseGen.getFilledImageData(screen, (x, y, value) => {
-        const h = value * 360 - 90;
+        const h = 180 + ((value * 210) + ((x / width) * 210)) / 2
         const s = 100;
-        const l = value * (85 - 45) + 45;
+        const l = 
+            (
+                (value * (85 - 45) + 45)
+                + ((1 - distance(x,y)) * (90 - 30) + 30)
+            ) / 2;
         const rgb = husl.toRGB(h, s, l);
         const bytes = rgb.map(x => Math.floor(x * 255));
         return bytes;
     });
     screen.context.putImageData(imageData, 0, 0);
+}
+
+function distanceFromCenter(width, height) {
+    const centerX = width / 2;
+    const centerY = height / 2;
+    const radius = Math.hypot(centerX, centerY);
+    const distance = (x,y) => {
+        return Math.hypot(x - centerX, y - centerY) / radius;
+    };
+    return distance;
 }
